cea5a592c571aa9cf40427705a4efac399020dd7
Amel Muminovic <amcenp@gmail.com>
2021-05-04 19:02:53 +0200

    Logic isolated

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,10 @@
 import express from "express";
-import { ApolloServer } from "apollo-server-express";
-import { typeDefs } from "./tempGQLTypes/index";
-import { resolvers } from "./tempResolvers/index";
 
 import registerMiddleware from "./middleware";
 
 const startServer = async () => {
-  const server = new ApolloServer({
-    // These will be defined for both new or existing servers
-    typeDefs,
-    resolvers,
-    context: ({ req, res }) => ({ req, res }),
-  });
-
   const app = express();
   registerMiddleware(app);
-  server.applyMiddleware({ app: app, path: "/graphql" });
   app.listen(3000, () => {
     console.log(`Server is up and running on port 3000`);
   });
--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,11 +1,22 @@
 import express, { Application, Request, Response } from "express";
 import cookieParser from "cookie-parser";
 
+import { ApolloServer } from "apollo-server-express";
+import { typeDefs } from "../tempGQLTypes/index";
+import { resolvers } from "../tempResolvers/index";
+
 export default (app: Application) => {
   app.use(express.json());
   app.use(cookieParser());
 
   // GraphQL Routes
+  const server = new ApolloServer({
+    // These will be defined for both new or existing servers
+    typeDefs,
+    resolvers,
+    context: ({ req, res }) => ({ req, res }),
+  });
+  server.applyMiddleware({ app: app, path: "/graphql" });
 
   // TODO API Routess
 
