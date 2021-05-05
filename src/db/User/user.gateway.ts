9c44704e359849b9a1d18f100b3e27e540dca87c
Amel Muminovic <amcenp@gmail.com>
2021-05-05 04:58:22 +0200

    Basic user gateway added

---
+++ src/db/User/user.gateway.ts
@@ -0,0 +1,12 @@
+// Todo Implement Real database
+
+class UserGateway {
+  constructor() {}
+  async getUserById(id: string | number) {
+    // Todo Map the data before returning
+    return {
+      name: "Amel",
+      age: 26,
+    };
+  }
+}
