9048d13b3aa4c4342942cd16cda3585cdf0af6bc
Amel Muminovic <amcenp@gmail.com>
2021-05-26 20:02:51 +0200

    Login Controller added

---
+++ src/modules/Auth/auth.controller.ts
@@ -0,0 +1,12 @@
+import { GQLArgs, GQLContext } from "../../types";
+import { Login } from "./use-cases";
+
+export const controllers = {
+  login: async (args: GQLArgs, context: GQLContext): Promise<string> => {
+    const loginUC = new Login({
+      userGateway: context.db.user,
+      input: args.input,
+    });
+    return await loginUC.login();
+  },
+};
