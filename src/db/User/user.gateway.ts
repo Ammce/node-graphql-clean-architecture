16882cdc8aa36c9a8a041a76942a07159c5983bf
Amel Muminovic <amcenp@gmail.com>
2021-05-26 20:07:25 +0200

    getUser by email implemented

--- src/db/User/user.gateway.ts
+++ src/db/User/user.gateway.ts
@@ -1,10 +1,11 @@
 // Todo Implement Real database
 
-import { userMock } from './user.mock';
-import { User } from './user.types';
+import { userMock } from "./user.mock";
+import { User } from "./user.types";
 
 export interface IUserGateway {
   getUserById(id: string | number): Promise<User>;
+  getUserByEmail(id: string): Promise<User>;
   getAllUsers(): Promise<User[]>;
   updateUser(id: string, data: any): Promise<User>;
   deleteUser(id: string): Promise<boolean>;
@@ -13,6 +14,11 @@ export interface IUserGateway {
 class UserGateway implements IUserGateway {
   constructor() {}
   async getUserById(id: string | number): Promise<User> {
+    // Todo Map the data before returning
+    return userMock;
+  }
+
+  async getUserByEmail(email: string): Promise<User> {
     // Todo Map the data before returning
     return userMock;
   }
