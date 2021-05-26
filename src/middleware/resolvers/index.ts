b82331dfaf0d7dd894cb9ea9b30b0d9c98c7ab3a
Amel Muminovic <amcenp@gmail.com>
2021-05-26 20:06:14 +0200

    Auth module hitting

--- src/middleware/resolvers/index.ts
+++ src/middleware/resolvers/index.ts
@@ -1,5 +1,7 @@
 import { resolvers as userResolvers } from "../../modules/User/user.index";
+import { resolvers as authResolvers } from "../../modules/Auth/auth.index";
 
 export default {
   ...userResolvers,
+  ...authResolvers,
 };
