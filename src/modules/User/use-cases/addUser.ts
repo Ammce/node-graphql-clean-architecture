44d32281e4b90065a248d1b79151c98e6324f3ef
Amel Muminovic <amcenp@gmail.com>
2021-05-26 19:55:06 +0200

    addUser corrected

--- src/modules/User/use-cases/addUser.ts
+++ src/modules/User/use-cases/addUser.ts
@@ -18,9 +18,6 @@ class AddUser {
 
   // Todo add return types of unique User type
   async addUser(): Promise<User> {
-    const { password, email } = this.input;
-    const hashedPassword = await bcrypt.hash(password, 10);
-    console.log(hashedPassword);
     return await this.userGateway.addUser(this.input);
   }
 }
